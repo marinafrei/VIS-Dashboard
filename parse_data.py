0f0403319b7170f56edf1e04bec4c23029501909
Marina Frei <marina.frei@stud.fhgr.ch>
2024-11-19 14:14:43 +0100

    read only chf columns

--- parse_data.py
+++ parse_data.py
@@ -2,12 +2,16 @@ import pandas as pd
 import plotly.express as px
 
 # Aufgabe 7a
-df_year = pd.read_excel('data.xlsx', sheet_name='Jahr', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20])
-df_age = pd.read_excel('data.xlsx', sheet_name='Altersklasse', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20])
-df_income = pd.read_excel('data.xlsx', sheet_name='Einkommen', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20])
-df_type = pd.read_excel('data.xlsx', sheet_name='Haushaltstyp', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20])
+df_year_chf = pd.read_excel('data.xlsx', sheet_name='Jahr', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20], usecols='A:G,I,K,M')
+#df_year_percent = pd.read_excel('data.xlsx', sheet_name='Jahr', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20], usecols='A:F,H,J,L,M')
+df_age_chf = pd.read_excel('data.xlsx', sheet_name='Altersklasse', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20], usecols='A:G,I,K,M,O,Q')
+#df_age_percent = pd.read_excel('data.xlsx', sheet_name='Altersklasse', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20], usecols='A:F,H,J,L,N,P,R')
+df_income_chf = pd.read_excel('data.xlsx', sheet_name='Einkommen', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20], usecols='A:G,I,K,M,O')
+#df_income_percent = pd.read_excel('data.xlsx', sheet_name='Einkommen', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20], usecols='A:F,H,J,L,N,P')
+df_type_chf = pd.read_excel('data.xlsx', sheet_name='Haushaltstyp', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20], usecols='A:G,I,K,M,O,Q')
+#df_type_percent = pd.read_excel('data.xlsx', sheet_name='Haushaltstyp', skiprows=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20], usecols= 'A:F,H,J,L,N,P,R')
 
-#Die Zellenbeschreibungen sind nicht alle in der ersten Spalte, sondern je nach Ebene eines eingerückt.
+#Die Zellenbeschreibungen sind nicht alle in der ersten Spalte, sondern je nach Ebene eins eingerückt.
 #Dies wird hier bereinigt und damit man die Info zur Ebene nicht verliert eine zusätzliche Spalte 'Ebene' eingefügt
 def clean_data(dataframe):
     dataframe.columns.values[5] = 'Ebene'
@@ -32,25 +36,17 @@ def clean_data(dataframe):
     dataframe.drop(['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1 , inplace=True)
     return dataframe
 
-df_year = clean_data(df_year)
-df_age = clean_data(df_age)
-df_income = clean_data(df_income)
-df_type = clean_data(df_type)
-
+df_year_chf = clean_data(df_year_chf)
+df_age_chf = clean_data(df_age_chf)
+df_income_chf = clean_data(df_income_chf)
+df_type_chf = clean_data(df_type_chf)
 
 
 #Aufgabe 7b
-column_names = df_age.columns.tolist()
-column_names_chf = []
-column_names_percent = []
-for column in column_names:
-    if 'CHF' in column:
-        column_names_chf.append(column)
-    if '%' in column:
-        column_names_percent.append(column)
 
-#funktioniert nicht
-fig = px.scatter(x=column_names_chf, y=df_age.loc['61: Gesundheitsausgaben'][1::2])
+column_names_chf = df_age_chf.columns.tolist()
+
+fig = px.scatter(x=column_names_chf[1:], y=df_age_chf.loc['61: Gesundheitsausgaben'][1:])
 fig.show()
 
 
