0b1a29dadfd41066fe765851588ff9c366755ca3
Marina Frei <marina.frei@stud.fhgr.ch>
2024-11-19 16:05:36 +0100

    changed format of df to long format and added second figure

--- parse_data.py
+++ parse_data.py
@@ -13,41 +13,48 @@ df_type_chf = pd.read_excel('data.xlsx', sheet_name='Haushaltstyp', skiprows=[0,
 
 #Die Zellenbeschreibungen sind nicht alle in der ersten Spalte, sondern je nach Ebene eins eingerückt.
 #Dies wird hier bereinigt und damit man die Info zur Ebene nicht verliert eine zusätzliche Spalte 'Ebene' eingefügt
-def clean_data(dataframe):
-    dataframe.columns.values[5] = 'Ebene'
-    dataframe['Ebene'] = dataframe['Ebene'].astype(object)
+def clean_data(dataframe, columnnames):
+    dataframe.rename(columns={dataframe.columns[0]: 'Kategorie', dataframe.columns[5]: 'Ebene'}, inplace= True)
+    dataframe['Ebene'] = dataframe['Ebene'].astype(object) #damit der datatype stimmt
     for index, row in dataframe.iterrows():
-        if pd.notna(row['Unnamed: 0']):
+        if pd.notna(row['Kategorie']):
             dataframe.loc[index, 'Ebene'] = '1'
         if pd.notna(row['Unnamed: 1']):
-            dataframe.loc[index, 'Unnamed: 0'] = row['Unnamed: 1']
+            dataframe.loc[index, 'Kategorie'] = row['Unnamed: 1']
             dataframe.loc[index, 'Ebene'] = '2'
         if pd.notna(row['Unnamed: 2']):
-            dataframe.loc[index, 'Unnamed: 0'] = row['Unnamed: 2']
+            dataframe.loc[index, 'Kategorie'] = row['Unnamed: 2']
             dataframe.loc[index, 'Ebene'] = '3'
         if pd.notna(row['Unnamed: 3']):
-            dataframe.loc[index, 'Unnamed: 0'] = row['Unnamed: 3']
+            dataframe.loc[index, 'Kategorie'] = row['Unnamed: 3']
             dataframe.loc[index, 'Ebene'] = '4'
         if pd.notna(row['Unnamed: 4']):
-            dataframe.loc[index, 'Unnamed: 0'] = row['Unnamed: 4']
+            dataframe.loc[index, 'Kategorie'] = row['Unnamed: 4']
             dataframe.loc[index, 'Ebene'] = '5'
 
-    dataframe.set_index(dataframe.columns[0], inplace=True)
     dataframe.drop(['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1 , inplace=True)
-    return dataframe
+    dataframe_long = dataframe.melt(id_vars=['Kategorie', 'Ebene'], var_name=columnnames, value_name='CHF')
+    return dataframe_long
 
-df_year_chf = clean_data(df_year_chf)
-df_age_chf = clean_data(df_age_chf)
-df_income_chf = clean_data(df_income_chf)
-df_type_chf = clean_data(df_type_chf)
+df_year_chf = clean_data(df_year_chf, 'Jahr')
+df_age_chf = clean_data(df_age_chf, 'Altersklasse')
+df_income_chf = clean_data(df_income_chf, 'Einkommensklasse')
+df_type_chf = clean_data(df_type_chf, 'Haushaltstyp')
 
 
 #Aufgabe 7b
+df_age_fig1 = df_age_chf[df_age_chf['Kategorie'] == '61: Gesundheitsausgaben']
+fig1 = px.scatter(df_age_fig1, x='Altersklasse', y='CHF')
+fig1.show()
 
-column_names_chf = df_age_chf.columns.tolist()
+selected_categories = ['5111: Brot und Getreideprodukte', '5112: Fleisch']
+df_age_fig2 = df_age_chf[df_age_chf['Kategorie'].isin(selected_categories)]
+fig2 = px.bar(df_age_fig2, x='Altersklasse', y='CHF', color='Kategorie', barmode='group')
+fig2.show()
 
-fig = px.scatter(x=column_names_chf[1:], y=df_age_chf.loc['61: Gesundheitsausgaben'][1:])
-fig.show()
+
+
+
 
 
 
