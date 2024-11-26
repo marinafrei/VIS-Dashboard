bf6c44b76913861e9d36a60c5e2e731d152b1070
Marina Frei <marina.frei@stud.fhgr.ch>
2024-11-26 11:33:52 +0100

    created file for dashboard, skipped some more unneeded rows

---
+++ dashboard.py
@@ -0,0 +1,55 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import plotly.express as px
+import numpy as np
+import pandas as pd
+import dash_bootstrap_components as dbc
+
+app = Dash (__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+rows_to_skip= list(range(0,13)) + [14,15,16,18,19,20] + list(range(553,583))
+df_year_chf = pd.read_excel('data.xlsx', sheet_name='Jahr', skiprows=rows_to_skip, usecols='A:G,I,K,M')
+df_age_chf = pd.read_excel('data.xlsx', sheet_name='Altersklasse', skiprows=rows_to_skip, usecols='A:G,I,K,M,O,Q')
+df_income_chf = pd.read_excel('data.xlsx', sheet_name='Einkommen', skiprows=rows_to_skip, usecols='A:G,I,K,M,O')
+df_type_chf = pd.read_excel('data.xlsx', sheet_name='Haushaltstyp', skiprows=rows_to_skip, usecols='A:G,I,K,M,O,Q')
+
+#Die Zellenbeschreibungen sind nicht alle in der ersten Spalte, sondern pro Ebene eins eingerückt.
+#Dies wird hier bereinigt und damit man die Info zur Ebene nicht verliert eine zusätzliche Spalte 'Ebene' eingefügt
+def clean_data(dataframe, columnnames):
+    dataframe.rename(columns={dataframe.columns[0]: 'Kategorie', dataframe.columns[5]: 'Ebene'}, inplace= True)
+    dataframe['Ebene'] = dataframe['Ebene'].astype(object) #damit der datatype stimmt
+    for index, row in dataframe.iterrows():
+        if pd.notna(row['Kategorie']):
+            dataframe.loc[index, 'Ebene'] = '1'
+        if pd.notna(row['Unnamed: 1']):
+            dataframe.loc[index, 'Kategorie'] = row['Unnamed: 1']
+            dataframe.loc[index, 'Ebene'] = '2'
+        if pd.notna(row['Unnamed: 2']):
+            dataframe.loc[index, 'Kategorie'] = row['Unnamed: 2']
+            dataframe.loc[index, 'Ebene'] = '3'
+        if pd.notna(row['Unnamed: 3']):
+            dataframe.loc[index, 'Kategorie'] = row['Unnamed: 3']
+            dataframe.loc[index, 'Ebene'] = '4'
+        if pd.notna(row['Unnamed: 4']):
+            dataframe.loc[index, 'Kategorie'] = row['Unnamed: 4']
+            dataframe.loc[index, 'Ebene'] = '5'
+
+    dataframe.drop(['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1 , inplace=True)
+    dataframe_long = dataframe.melt(id_vars=['Kategorie', 'Ebene'], var_name=columnnames, value_name='CHF') #Hier wird der df in das Long-Format geändert, da man so viel einfacher Diagramme mit plotly erstellen kann.
+    return dataframe_long
+
+df_year_chf = clean_data(df_year_chf, 'Jahr')
+df_age_chf = clean_data(df_age_chf, 'Altersklasse')
+df_income_chf = clean_data(df_income_chf, 'Einkommensklasse')
+df_type_chf = clean_data(df_type_chf, 'Haushaltstyp')
+
+print(df_age_chf['Kategorie'])
+
+"""
+app.layout = html.Div([html.H1("Dashboard Haushaltsausgaben"),
+    dbc.Col
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
+"""
